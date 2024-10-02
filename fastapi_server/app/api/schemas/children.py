2f65f87473342c3985313fa3f43127cce61b2eff
Christina Frezynski <christina@frez.me>
2024-10-02 14:01:34 -0500

    update update schemas

--- fastapi_server/app/api/schemas/children.py
+++ fastapi_server/app/api/schemas/children.py
@@ -4,12 +4,15 @@ import datetime as dt
 import uuid
 from typing import Optional
 
+from pydantic import ConfigDict
+
 from .base import BaseSchema, IDSchemaMixin
 
 # from.parents import ParentInDB
 
 
 class ChildBase(BaseSchema):
+    """Base schema for 'child'."""
     name: str
     birthdate: Optional[dt.date]
     hobby: Optional[str]
@@ -23,12 +26,22 @@ class ChildInDB(ChildBase, IDSchemaMixin):
 
 
 class ChildCreate(ChildBase):
+    """Schema for creating 'child' in database."""
     pass
 
 
-class ChildUpdate(ChildInDB):
-    pass
+class ChildUpdate(ChildBase):
+    """Schema for updating 'child' in database.
 
+    Updating only allowed for 'name', 'birthdate', 'hobby', 'parent_id' fields.
+    All other fields will be ignored.
+    """
+    model_config = ConfigDict(extra='ignore') 
+    
+    name : str | None = None
+    birthdate: dt.date | None = None
+    hobby: str | None = None
+    parent_id: uuid.UUID | None = None
 
 class ChildWithParent(ChildInDB):
     # parent: ParentInDB
