334e651ecf37f500c3a9639d469a8b3f3587f5a6
Christina Frezynski <christina@frez.me>
2024-10-02 13:43:52 -0500

    update frontend to match backend model for parent

--- lib/src/parent/parent_model.dart
+++ lib/src/parent/parent_model.dart
@@ -2,6 +2,8 @@ import 'package:family/src/children/child.dart';
 
 class Parent {
   String? id;
+  DateTime? createdAt;
+  DateTime? updatedAt;
   String name = '';
   DateTime? birthdate;
   List<Child> children = [];
@@ -11,12 +13,19 @@ class Parent {
   }
 
   Parent.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    createdAt = DateTime.parse(json['created_at']);
+    updatedAt = DateTime.parse(json['updated_at']);
     name = json['name'];
-    birthdate = DateTime.parse(json['birthdate']);
+    // TODO: Verify if this is the correct way to parse the date only from backend
+    birthdate = json['birthdate'] ? DateTime.parse(json['birthdate']) : null;
   }
 
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
+      'created_at': createdAt.toString(),
+      'updated_at': updatedAt.toString(),
       'name': name,
       'birthdate': birthdate.toString(),
     };
