249ee98f1ac91eeb7d364456b22957117dbbfdcd
Christina Frezynski <christina@frez.me>
2024-10-02 09:20:38 -0500

    add base model to base filter

--- fastapi_server/app/api/filters/base.py
+++ fastapi_server/app/api/filters/base.py
@@ -5,10 +5,12 @@ Provides base filters for what is in the BaseSchema.
 import uuid
 from datetime import datetime
 
-from fastapi_filter.contrib.sqlalchemy import Filter as SQLAlchemyFilter
+from fastapi_filter.contrib.sqlalchemy import Filter
+
+from app.db.models.base import BaseModel
 
 
-class BaseFilter(SQLAlchemyFilter):
+class BaseFilter(Filter):
     id: uuid.UUID | None = None
     id__in: list[uuid.UUID] | None = None
     # These could be extended to include `_at`, `_at__lt`, `_at__gt for more
@@ -20,3 +22,6 @@ class BaseFilter(SQLAlchemyFilter):
 
     # Default ordering by updated_at with most recent first
     order_by: list[str] = ["-updated_at"]
+
+    class Constants(Filter.Constants):
+        model = BaseModel
